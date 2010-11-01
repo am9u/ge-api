10a3ab2219e72aa5fe44a44449e2e57cb1210b3d
Jon Ferrer <jon@jonferrer.com>
2010-10-31 23:40:01 -0400

    add venue model + validation and convenience functionality for creating related address models

--- application/classes/model/venue.php
+++ application/classes/model/venue.php
@@ -4,24 +4,34 @@ class Model_Venue extends ORM
 {
     protected $_db = 'event_warehouse';
 
+    // relationships
     protected $_has_many = array(
+        'tags' => array('through' => 'tagmaps'),
         'addresses' => array('through' => 'venue_addresses', 'foreign_key' => 'venue_id', 'far_key' => 'address_id'),
         'events' => array()
     );
+
+    // validation
+    protected $_rules = array(
+        'name' => array('not_empty' => array()),
+    );
     
+    // @TODO: add primary address support
     // protected function address()
     // {
     //     return $this->addresses->where('is_primary', '=', TRUE)->find();
     // }
 
-    private $_address = NULL;
+    private $_address = NULL; // related address. used by Model_Venue::values() for storing address data
 
+    /**
+     * Creates/updates values for related models, then calls parent::values()
+     */
     public function values($values)
     {
-        Kohana::$log->add('Model_Venue->values()', 'running overwritten method');
-
-        Kohana::$log->add('Model_Venue->values()', 'found adr_ in $key');
+        // find address values in $values array
         $adr = array();
+
         foreach($values as $key => $val)
         {
             if(strpos($key, 'adr_') !== false)
@@ -31,32 +41,74 @@ class Model_Venue extends ORM
             }
         }
 
+        // check if address values were found
         if(count($adr) > 0)
         {
+            // find address
+            // @TODO: refactor to handle multiple and/or primary addresses
             if(isset($adr->id))
             {
                 Kohana::$log->add('Model_Venue->values()', 'found address id');
-                $address = $this->addresses->find();
-                $address->values($adr);
+                $this->_address = $this->addresses->find();
             }
+            // create address
             else 
             {
                 Kohana::$log->add('Model_Venue->values()', 'create address');
-                $this->_address = ORM::factory('address')->values($adr)->save();
+                $this->_address = ORM::factory('address');
             }
+
+            // assign address values in related model
+            $this->_address->values($adr);
         }
 
         return parent::values($values);
     }
 
+    /**
+     * Validates related models 
+     */
+    public function check()
+    {
+
+        if($this->_address !== NULL AND $this->_address->check() === FALSE)
+        {
+            return FALSE;
+        }
+
+        return parent::check();
+    }
+
+    /**
+     * Saves and adds related models before saving Model data
+     */
     public function save()
     {
         parent::save();
 
+        // addresses
         if( ! empty($this->_address)) 
         {
+            $this->_address->save();
             $this->add('addresses', $this->_address);
         }
+
+        // if event is created (not updated), generate it's unique event tag
+        if($this->tags->where('name', '=', $this->name.': '.$this->id)->count_all() < 1) {
+            $core_tag_name = ucfirst($this->_object_name);
+
+            $event_parent_tag = ORM::factory('tag')
+                                    ->core_tag($core_tag_name)
+                                    ->find();
+
+            // autocreate internal tag for this event. eventually we should tagname to be event.name_event.date, ie: A Fan Ti_2010.11.01... or something like that
+            $event_tag = ORM::factory('tag')
+                            ->values(array('name' => $this->name.': '.$this->id, 'parent_id' => $event_parent_tag->id))
+                            ->save();
+
+            $this->add('tags', $event_tag); 
+        }
+
         // if( ! empty($this->_tag)) 
         // {
         //     $this->add('tags', $this->_tag);
