a2bef8d8ce4693439341a891e469e0456a831589
Jon Ferrer <jon@jonferrer.com>
2010-10-31 23:28:07 -0400

    add address model + validation rules

--- application/classes/model/address.php
+++ application/classes/model/address.php
@@ -4,7 +4,21 @@ class Model_Address extends ORM
 {
     protected $_db = 'event_warehouse';
 
-    protected $_has_many = array('venues' => array('through' => 'venue_address', 'foreign_key' => 'address_id', 'far_key' => 'venue_id'));
+    // relationships
+    protected $_has_many = array(
+        'venues' => array('through' => 'venue_address', 'foreign_key' => 'address_id', 'far_key' => 'venue_id')
+    );
+
+    // rules
+    protected $_rules = array(
+        'line_1' => array('not_empty' => array()),
+        'city' => array('not_empty' => array()),
+        'state_province' => array('not_empty' => array()),
+        'zip' => array(
+            'not_empty' => array(),
+            'min_length' => array(5)
+        ),
+    );
 
 }
 
