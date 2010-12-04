7194fc106789ddfbe9d241bc87797c093071a367
Jon Ferrer <jon@jonferrer.com>
2010-12-04 13:23:00 -0500

    move cache orm and mongo code to separate branch

--- application/classes/controller/event.php
+++ application/classes/controller/event.php
@@ -13,7 +13,7 @@ class Controller_Event extends Controller_REST
      */
     public function action_recent($limit = 3)
     {
-        $events = CacheORM::factory($this->_model_type)->order_by('datetime', 'desc')->limit($limit);
+        $events = ORM::factory($this->_model_type)->order_by('datetime', 'desc')->limit($limit);
         $this->_payload = $events->find_all();
 
         $this->_status = array(
@@ -22,56 +22,4 @@ class Controller_Event extends Controller_REST
             'message' => 'OK'
         );
     }
-
-    // MongoDB insert
-    public function action_create()
-    {
-        // only needed if post is coming from CURL?
-        //$this->_data = $this->_parse_form_data($_POST);
-        $this->_data = $_POST;
-
-        // xss security
-        $this->_data = $this->sanitize_values($this->_data);
-
-        // $venue = new Model_Venue();
-        // $venue->set('name', $this->_data['venue_name']);
-        // $venue->set('address_1', $this->_data['address_1']);
-        // $venue->set('address_2', $this->_data['address_2']);
-        // $venue->save();
-
-        $event = new Model_Event();
-
-        $event->name = $this->_data['name'];
-        $event->description = $this->_data['description'];
-        $event->date = new MongoDate(); // $this->_data['date'];
-        //$event->venue = $venue;
-
-        $event->tags = array(
-            uniqid().'_'.$event->name.'_'.$event->date
-         );
-
-        $event->save();
-
-        $this->_status = array(
-            'type'    => 'success',
-            'code'    => '200',
-            'message' => 'OK'
-        );
-
-        // $this->_payload = $event;
-    }
-
-    public function action_index($id=NULL)
-    {
-        $event = new Model_Event($id);
-        $event->load();
-
-        $date = date('Y-M-d h:i:s', $event->date->sec); 
-        $this->_status = array(
-            'type'    => 'success',
-            'code'    => '200',
-            'message' => 'OK. '.$date
-        );
-    }
-
 }
