1f97c753ece0f7af8e9c3549a2834f920ade32ff
Jon Ferrer <jon@jonferrer.com>
2010-12-10 18:19:01 -0500

    enable auth module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -77,7 +77,7 @@ Kohana::$config->attach(new Kohana_Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	// 'auth'       => MODPATH.'auth',       // Basic authentication
+	'auth'       => MODPATH.'auth',       // Basic authentication
 	'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
 	'database'   => MODPATH.'database',   // Database access
