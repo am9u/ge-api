45edbf87b41475931084ab9df45ec1a783197c8a
Jon Ferrer <jon@jonferrer.com>
2010-12-20 22:37:41 -0500

    clean up find by name bug in group controller

--- application/classes/controller/group.php
+++ application/classes/controller/group.php
@@ -5,7 +5,8 @@ class Controller_Group extends Controller_REST
     protected $_model_type = 'group';
 
     protected $_valid_get_actions = array(
-        'base' => array()
+        'base' => array(),
+        'get_by_name' => array(),
     );
 
     public function action_create()
@@ -36,20 +37,33 @@ class Controller_Group extends Controller_REST
     public function action_base()
     {
         $groups = ORM::factory($this->_model_type)->base_groups();
+        $this->_payload = $groups;
 
         $this->_status = array(
             'type'    => 'success',
             'code'    => '200',
         );
 
-        if($groups->count_all() === 1)
+    }
+
+    public function action_get_by_name($name)
+    {
+        $name = str_replace('-', ' ', $name);
+
+        $this->_model = ORM::factory($this->_model_type)->where('name', '=', $name);
+
+        $num_groups = $this->_model->count_all();
+        Kohana::$log->add('debug', 'Controller_Group::action_get_by_name() -- $name='.$name.' num rows='.$num_groups);
+
+        if($num_groups === 1)
         {
-            $this->_payload = $groups->find();
-        }
-        else
-        {
-            $this->_payload = $groups->find_all();
+            Kohana::$log->add('debug', 'Controller_Group::action_get_by_name() -- found group by name. setting $this->_payload.');
+            $this->_payload = $this->_model->where('name', '=', $name);
         }
 
+        $this->_status = array(
+            'type'    => 'success',
+            'code'    => '200',
+        );
     }
 }
