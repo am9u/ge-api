9f782b6d63a9d50de2855e2a5801999f5926ca9c
Jon Ferrer <jon@jonferrer.com>
2010-12-20 22:38:32 -0500

    add find events by group RPC

--- application/classes/controller/event.php
+++ application/classes/controller/event.php
@@ -6,7 +6,19 @@ class Controller_Event extends Controller_REST
 
     protected $_valid_get_actions = array(
         'recent' => array(),
+        'by_group' => array(),
     );
+
+    public function action_create()
+    {
+        parent::action_create();
+
+        if(isset($this->_data['group_id']))
+        {
+            $group = ORM::factory('group', $this->_data['group_id']);
+            $this->_model->add('groups', $group);
+        }
+    }
 
     /**
      * An example of a custom RPC action. This isn't really REST because the action is other than index.
@@ -15,6 +27,19 @@ class Controller_Event extends Controller_REST
     {
         $events = ORM::factory($this->_model_type)->order_by('datetime', 'desc')->limit($limit);
         $this->_payload = $events->find_all();
+
+        $this->_status = array(
+            'type'    => 'success',
+            'code'    => '200',
+            'message' => 'OK'
+        );
+    }
+
+    public function action_by_group($group_id)
+    {
+        $events = ORM::factory('group', $group_id)->events;
+
+        $this->_payload = $events;
 
         $this->_status = array(
             'type'    => 'success',
