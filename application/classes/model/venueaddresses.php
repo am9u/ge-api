a81652f2f0c5a9dc21028d62ed9028650a0d01a0
Jon Ferrer <jon@jonferrer.com>
2011-01-06 12:46:58 -0500

    clean up unused venue addresses model

--- application/classes/model/venueaddresses.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Model_Venue_Addresses extends ORM 
-{
-    protected $_belongs_to = array('venue' => array(), 'address' => array());
-}
